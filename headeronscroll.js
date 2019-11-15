085ba89d37c584f985f05e105c83ae7038301377
harrybless11 <53966910+harrybless11@users.noreply.github.com>
2019-11-15 10:00:59 +0000

    Add files via upload
    my first project..

---
+++ headeronscroll.js
@@ -0,0 +1,12 @@
+window.onscroll = function()  {myFunction()};
+
+let header = document.getElementById("myNavHeader");
+let sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
